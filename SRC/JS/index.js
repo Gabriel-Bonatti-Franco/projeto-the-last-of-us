e22e044bde705daa2e511e4c879346c0ac1c83de
Gabriel-Bonatti-Franco <127668312+Gabriel-Bonatti-Franco@users.noreply.github.com>
2023-03-12 09:51:12 -0300

    Add files via upload

---
+++ SRC/JS/index.js
@@ -0,0 +1,78 @@
+/*
+    
+    OBJETIVO - Quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
+
+    - passo 1 - dar um jeito de pegar o elemento HTML dos botoes
+
+    - passo 2 - dar um jeito de identificar o click do usuário no botão
+
+    - passo 3 - desmarcar o botão selecionado anterior
+    
+    - passo 4 - marcar o botão clicado como selecionado
+
+    - passo 5 - Esconder a imagem anterior
+
+    - passo 6 fazer aparecer a imagem correspondente ao botão clicado
+
+*/
+
+// passo 1 - dar um jeito de pegar o elemento HTML dos botoes
+
+const botoesCarrossel = document.querySelectorAll('.botao')
+const imagensCarrossel = document.querySelectorAll('.imagem')
+
+// passo 2 - dar um jeito de identificar o click do usuário no botão
+
+botoesCarrossel.forEach((botao, index) =>{
+    botao.addEventListener('click', () => {
+        
+        desativarBotaoSelecionado()
+        selecionarBotao(botao)
+        desativarImagemAtual()
+        selecionarImagemFundo(index)
+
+        clearInterval(meucarrossel)
+        carrosselAutomatico(index)
+    })
+})
+
+function selecionarImagemFundo(index) {
+    imagensCarrossel[index].classList.add('ativa')
+}
+
+function selecionarBotao(botao) {
+    botao.classList.add('selecionado')
+}
+
+function desativarImagemAtual() {
+    const imagemSelecionada = document.querySelector('.ativa')
+    imagemSelecionada.classList.remove('ativa')
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado')
+    botaoSelecionado.classList.remove('selecionado')
+}
+
+function carrosselAutomatico(index) {
+    
+    let proximo = index +  1
+    
+    meucarrossel = setInterval(() => {
+
+        desativarBotaoSelecionado()
+        selecionarBotao(botoesCarrossel[proximo])
+        desativarImagemAtual()
+        selecionarImagemFundo(proximo)
+
+        if (proximo < (botoesCarrossel.length - 1)){
+            proximo = proximo +  1
+        }else{
+            proximo = 0
+        }
+
+    }, 2500)
+
+}
+
+document.addEventListener('onLoad', carrosselAutomatico(0))
